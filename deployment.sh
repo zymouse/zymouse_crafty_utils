b070f8aa3ef003e2d9a1cf353090a0aaf2c59f91
zymouse <zymouse@pixmoving.net>
2023-12-27 18:07:35 +0800

    fix:部署脚本优化

--- deployment.sh
+++ deployment.sh
@@ -4,6 +4,12 @@ source $SCRIPT_DIR/script/create_rule_files/log.sh
 
 
 function install_yq() {
+    version_info=$(yq --version)
+    if [ $? -eq 0 ]; then
+        extracted_version=$(echo $version_info | grep -oP 'version \Kv[0-9.]+')
+    fi
+    extracted_version=""
+
     set -e #如果任何语句的执行结果不是真（即返回非零退出状态）
     arch=$(uname -m)
 
@@ -20,12 +26,18 @@ function install_yq() {
             ;;
     esac
 
+    # 判断版本号是否为 v4.35.2
+    if [ "$extracted_version" != "$VERSION" ]; then
+        return 0
+    fi
+
     VERSION=v4.35.2 
     BINARY=yq_linux_$arch
 
     wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} &&\
     sudo mv ${BINARY} /usr/bin/yq &&\
     sudo chmod +x /usr/bin/yq
+    return 0
 }
 
 
