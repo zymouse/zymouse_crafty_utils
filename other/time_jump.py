6d1e24c3f3249125123b1c6375e2f4c1bc774a85
zymouse <zymouse@pixmoving.net>
2025-09-04 10:55:38 +0800

    fix(other): 优化时间跳变监控脚本-跳变时查看时间源

--- other/time_jump.py
+++ other/time_jump.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 import time
 import signal
+import subprocess
+import platform
+import os
 
 # 输出文件
-LOG_FILE = "/tmp/time_jump.log"
+# 获取系统架构
+arch = platform.machine().lower()
+if "arm" in arch or "aarch64" in arch:
+    LOG_FILE = "/home/nvidia/pix/ros2bag/time_jump.log"
+elif "x86_64" in arch or "amd64" in arch:
+    LOG_FILE = "/home/ipc/pix/ros2bag/time_jump.log"
+else:
+    LOG_FILE = "/tmp/time_jump.log"  # 默认路径
+# 确保目录存在
+os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
+print("当前架构:", arch)
+print("日志路径:", LOG_FILE)
+
 
 # 跳变阈值（秒）
 THRESHOLD = 0.001
@@ -17,6 +32,17 @@ def signal_handler(sig, frame):
     global running
     print("\n[INFO] 捕获 Ctrl+C，正在退出...")
     running = False
+
+def get_time_source():
+    try:
+        result = subprocess.check_output(
+            "chronyc sources | awk '/\\*/ {print $2}'",
+            shell=True,
+            text=True
+        ).strip()
+        return result if result else "Unknown"
+    except Exception as e:
+        return f"Error: {e}"
 
 
 def main():
@@ -39,7 +65,8 @@ def main():
 
             # 如果首次运行 或者 差值变化超过阈值，写日志
             if last_diff is None or abs(diff - last_diff) > THRESHOLD:
-                log_line = f"{time.strftime('%Y-%m-%d %H:%M:%S')} diff={diff:.6f}s\n"
+                time_source = get_time_source()
+                log_line = f"{time.strftime('%Y-%m-%d %H:%M:%S')} diff={diff:.6f}s, 时间源:{time_source}\n"
                 f.write(log_line)
                 f.flush()  # 立即写盘
                 print(log_line, end="")
